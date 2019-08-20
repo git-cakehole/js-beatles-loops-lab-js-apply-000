4662897771fe60a1f6f9fe895d21097f2d9b3591
dandy-dispatch-5270 <kylepcole@gmail.com>
2019-08-20 15:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var output = [];
+
+  for (var i=0; i < musicians.length; i++) {
+    output.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return output;
+}
+
+function johnLennonFacts(array) {
+  for (var i=0; i < array.length; i++){
+    array[i] = array[i] + '!!!';
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number){
+  var output = [];
+  var i = 1;
+  while (i < number) {
+    output.push("I love the beatles!");
+  }
+}
